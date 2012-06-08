0bc1befb8cdfadb26aa385b0d07d9126bad804ee
cnsdato <d.toplak@cadenas.de>
2012-06-08 11:34:53 +0200

    mod: android works now, but not 100% as we want -> only when fresh starting the app it works, when calling via another url and resuming the app, we get the old url, see: http://www.madewithmarmalade.com/devnet/forum/5947#comment-28999

--- source/android/CustomURLHandler.java
+++ source/android/CustomURLHandler.java
@@ -10,6 +10,12 @@ These functions are called via JNI from native code.
  * be overwritten (unless --force is specified) and is intended to be modified.
  */
 import com.ideaworks3d.marmalade.LoaderAPI;
+import com.ideaworks3d.marmalade.LoaderActivity;
+import com.ideaworks3d.marmalade.LoaderThread;
+import android.net.Uri;
+import android.content.Intent;
+import java.lang.reflect.Field;
+import java.lang.String;
 
 class CustomURLHandler
 {
@@ -19,7 +25,40 @@ class CustomURLHandler
     }
     
     public String CustomURLHandlerGetURL()
-    {
+    {      
+      try
+      { 
+        
+        Class activitiyClass = Class.forName("com.ideaworks3d.marmalade.LoaderActivity");        
+        Field activitiyField = activitiyClass.getField("m_Activity");                
+        LoaderActivity activitiyObject = (LoaderActivity)activitiyField.get(activitiyClass);        
+        Intent intent = activitiyObject.getIntent();
+        
+        
+        /*
+        Class LoaderThreadClass = Class.forName("com.ideaworks3d.marmalade.LoaderActivity");
+        Field LoaderThreadSingleton = LoaderThreadClass.getField("g_Singleton");
+        LoaderThread loaderthread = (LoaderThread)LoaderThreadSingleton.get(LoaderThreadClass);
+        
+        Field LoaderActivityMember = LoaderThreadClass.getDeclaredField("m_Loader");
+        LoaderActivityMember.setAccessible(true); // hack to access private member
+        LoaderActivity activitiyObject = (LoaderActivity)LoaderActivityMember.get(loaderthread);       
+        
+        Intent intent = activitiyObject.getIntent();
+        */
+        
+        Uri uri = intent.getData();
+        return uri.toString();
+      }
+      catch(Exception e)
+      {
+        System.out.println(e.getMessage());
+      }
+      //return new String("-N-O---U-R-L-");
+      //return new "-N-O---U-R-L-";
+      //return "-N-O---U-R-L-";
+      //return new String("");
+      //return "";
       return null;
     }
 }
--- source/android/CustomURLHandler_platform.cpp
+++ source/android/CustomURLHandler_platform.cpp
@@ -27,44 +27,60 @@ struct JavaString
 
     JavaString ( const char *Buffer )
     {
+        //IwTrace(CUSTOMURLHANDLER, ("CTOR: const char *Buffer"));
         JNIEnv *env = s3eEdkJNIGetEnv();
         _Ref = env->NewStringUTF ( Buffer );
     }
 
     JavaString ( jstring Ref )
     {
+        //IwTrace(CUSTOMURLHANDLER, ("CTOR: jstring Ref"));
         _Ref = Ref;
     }
 
     ~JavaString()
     {
+        //IwTrace(CUSTOMURLHANDLER, ("DTOR"));
         JNIEnv *env = s3eEdkJNIGetEnv();
         env->DeleteLocalRef ( _Ref );
     }
 
     operator jstring()
     {
+        //IwTrace(CUSTOMURLHANDLER, ("Operator"));
         return _Ref;
     }
 };
 
 const char* getCString(jstring str)
 {
+    //IwTrace(CUSTOMURLHANDLER, ("getCString"));
     JNIEnv* env = s3eEdkJNIGetEnv();
     if (!str)
         return NULL;
+    //IwTrace(CUSTOMURLHANDLER, ("valid string"));
     jboolean free;
     const char* res = env->GetStringUTFChars(str, &free);
+    //IwTrace(CUSTOMURLHANDLER, ("GetStringUTFChars"));
     g_RetStrLen = strlen(res);
-    s3eEdkReallocOS(g_RetStr, g_RetStrLen);
-    strncpy(g_RetStr, res, g_RetStrLen);
+    //IwTrace(CUSTOMURLHANDLER, ("strlen=%d", g_RetStrLen));
+    s3eEdkReallocOS(g_RetStr, g_RetStrLen+1);
+    g_RetStr[g_RetStrLen] = '\0';
+    //IwTrace(CUSTOMURLHANDLER, ("s3eEdkReallocOS"));
+    //IwTrace(CUSTOMURLHANDLER, ("cpy '%s'", res));
+    strncpy(g_RetStr, res, g_RetStrLen);    
+    //IwTrace(CUSTOMURLHANDLER, ("strncpy"));
     env->ReleaseStringUTFChars(str, res);
+    //IwTrace(CUSTOMURLHANDLER, ("ReleaseStringUTFChars"));
     return g_RetStr;
 }
 
 
 s3eResult CustomURLHandlerInit_platform()
 {
+    //Alloc buffer for returning strings
+    g_RetStr = (char*)s3eEdkMallocOS(g_RetStrLen);
+    
     // Get the environment from the pointer
     JNIEnv* env = s3eEdkJNIGetEnv();
     jobject obj = NULL;
@@ -89,7 +105,7 @@ s3eResult CustomURLHandlerInit_platform()
     g_CustomURLHandlerRegister = env->GetMethodID(cls, "CustomURLHandlerRegister", "()V");
     if (!g_CustomURLHandlerRegister)
         goto fail;
-    g_CustomURLHandlerGetURL = env->GetMethodID(cls, "CustomURLHandlerGetURL", "()V");
+    g_CustomURLHandlerGetURL = env->GetMethodID(cls, "CustomURLHandlerGetURL", "()Ljava/lang/String;");
     if (!g_CustomURLHandlerGetURL)
         goto fail;
 
@@ -118,6 +134,8 @@ fail:
 void CustomURLHandlerTerminate_platform()
 {
     // Add any platform-specific termination code here
+    s3eEdkFreeOS(g_RetStr);
+    g_RetStr = NULL;
 }
 
 void CustomURLHandlerRegister_platform(CustomURLHandlerCallback fn)
@@ -129,6 +147,8 @@ void CustomURLHandlerRegister_platform(CustomURLHandlerCallback fn)
 const char* CustomURLHandlerGetURL_platform()
 {
     JNIEnv* env = s3eEdkJNIGetEnv();
+    //IwTrace(CUSTOMURLHANDLER, ("BEFORE_CALL"));
     JavaString str((jstring)env->CallObjectMethod(g_Obj, g_CustomURLHandlerGetURL));
+    //IwTrace(CUSTOMURLHANDLER, ("AFTER_CALL"));
     return getCString((jstring)str);
 }
